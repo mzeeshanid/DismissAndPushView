9fc9ea8d5c7931251b26d0410576a68d77dd9b42
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2024-07-24 00:59:34 +0500

    Initial Commit

---
+++ DismissAndPushView/DismissAndPushViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DismissAndPushViewApp.swift
+//  DismissAndPushView
+//
+//  Created by Ali Aslam on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct DismissAndPushViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
